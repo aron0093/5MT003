1037244824c3c91ce2446770a1ca41e9d1a7d860
Aron0093 <aron0093@gmail.com>
2018-01-11 17:58:35 +0000

    Updated script to use actual data

--- DanQ_train_keras2.1.1.py
+++ DanQ_train_keras2.1.1.py
@@ -14,24 +14,22 @@ from keras.optimizers import RMSprop
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Conv1D, MaxPooling1D
-#from keras.regularizers import l2, activity_l1
 from keras.constraints import maxnorm
 from keras.layers.recurrent import LSTM
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.layers import Bidirectional
-#from keras.utils.layer_utils import print_layer_shapes
 
 
 print('loading data')
-#trainmat = h5py.File('data/train.mat')
-#validmat = scipy.io.loadmat('data/valid.mat')
-#testmat = scipy.io.loadmat('data/test.mat')
+trainmat = h5py.File('data/train.mat')
+validmat = scipy.io.loadmat('data/valid.mat')
+testmat = scipy.io.loadmat('data/test.mat')
 
-#X_train = np.transpose(np.array(trainmat['trainxdata']),axes=(2,0,1))
-#y_train = np.array(trainmat['traindata']).T
+X_train = np.transpose(np.array(trainmat['trainxdata']),axes=(2,0,1))
+y_train = np.array(trainmat['traindata']).T
 
-X_train = np.ones((500,1000,4))
-y_train = np.zeros((500,919))
+#X_train = np.ones((500,1000,4))
+#y_train = np.zeros((500,919))
 
 
 lstm = LSTM(units=320, return_sequences=True)
@@ -66,12 +64,12 @@ print('running at most 60 epochs')
 checkpointer = ModelCheckpoint(filepath="DanQ_bestmodel.hdf5", verbose=1, save_best_only=True)
 earlystopper = EarlyStopping(monitor='val_loss', patience=5, verbose=1)
 
-model.fit(X_train, y_train, batch_size=100, nb_epoch=60, shuffle=True, verbose=2 #show_accuracy=True,
-          #validation_data=(np.transpose(validmat['validxdata'],axes=(0,2,1)), validmat['validdata']),
-          #callbacks=[checkpointer,earlystopper]
+model.fit(X_train, y_train, batch_size=100, epochs=60, shuffle=True, verbose=2,
+          validation_data=(np.transpose(validmat['validxdata'],axes=(0,2,1)), validmat['validdata']),
+          callbacks=[checkpointer,earlystopper]
           )
 
-#tresults = model.evaluate(np.transpose(testmat['testxdata'],axes=(0,2,1)), testmat['testdata'],show_accuracy=True)
+tresults = model.evaluate(np.transpose(testmat['testxdata'],axes=(0,2,1)), testmat['testdata'],show_accuracy=True)
 
-#print (tresults)
+print (tresults)
 
